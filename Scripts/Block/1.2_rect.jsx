16ecb44a65d45fc27688dc4ef39c41323ef88bd4
Doeroe <dorothee.schraudner@gmail.com>
2017-04-23 09:49:41 +0200

    Add files via upload

---
+++ Scripts/Block/1.2_rect.jsx
@@ -0,0 +1,12 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+
+  b.clear(b.doc());
+  b.units(b.MM);
+  b.noFill();
+
+  b.rect(10, 10, 100, 100)
+}
+b.go();
