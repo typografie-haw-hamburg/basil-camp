b4e4df67523fbe891334ac333524ac4a87f8ef1b
Doeroe <dorothee.schraudner@gmail.com>
2017-04-23 09:25:56 +0200

    Delete 1.2_rect.jsx

--- Scripts/1.2_rect.jsx
+++
@@ -1,1 +0,0 @@
-﻿#includepath "~/Documents/;%USERPROFILE%Documents";#include "basiljs/bundle/basil.js";function draw() {  b.clear(b.doc());  b.units(b.MM);  b.noFill();  b.rect(10, 10, 100, 100)}b.go();
