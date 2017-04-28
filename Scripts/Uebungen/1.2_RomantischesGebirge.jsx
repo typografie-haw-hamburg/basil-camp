10cbdc98a49a523cbc110f0843ceef7427e47d53
Doeroe <dorothee.schraudner@gmail.com>
2017-04-28 14:40:46 +0200

    Add files via upload

---
+++ Scripts/Uebungen/1.2_RomantischesGebirge.jsx
@@ -0,0 +1,16 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+
+b.noFill();
+b.clear(b.doc());
+
+b.line(80, 130, 100, 100);
+b.line(100, 100, 150, 150);
+b.line(150, 150, 180, 90);
+b.line(180, 90, 220, 160);
+
+}
+
+b.go();
---
+++ Scripts/Uebungen/5.4_b.Text.jsx
@@ -0,0 +1,12 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+  b.units(b.MM);
+  b.rectMode(b.CENTER);
+
+  var tf = b.selection();
+  var words = b.words(tf);
+
+}
+b.go();
