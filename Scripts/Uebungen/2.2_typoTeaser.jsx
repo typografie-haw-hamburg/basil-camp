555bfe7b6e7a48870f71d09e07b357e53fe07930
trych <timorychert@23x5.net>
2017-05-04 12:01:07 +0200

    Removed old exercises

--- Scripts/Uebungen/2.2_typoTeaser.jsx
+++
@@ -1,17 +0,0 @@
-﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
-// @include "basiljs/basil.js";
-
-function draw() {
-
-b.clear(b.doc());
-
-var myText = "I LOVE ROBO"; 
-var xPos = 20; var yPos = 20; 
-var widthTextframe = 150; 
-var heightTextframe = 30; 
-
-b.text(myText, xPos, yPos, widthTextframe, heightTextframe);
-
-}
-
-b.go();
