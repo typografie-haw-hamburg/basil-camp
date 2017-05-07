38bcd5d8cfdb2a66c63d4d30928c1510c2a977c8
Doeroe <dorothee.schraudner@gmail.com>
2017-05-07 17:36:35 +0200

    Add files via upload

---
+++ Scripts/Uebungen/7.1_thumb.jsx
@@ -0,0 +1,29 @@
+﻿// @includepath "~/Documents/;%USERPROFILE%Documents";
+// @include "basiljs/basil.js";
+
+function draw() {
+
+  b.clear(b.doc());
+  b.noStroke();
+
+
+  for (var i = 1; i < 10; i++) {
+    var img = b.image("mirror_" + i + ".png", 0, 0)
+
+    var imgWidth = b.itemWidth(img);
+    var imgHeight = b.itemHeight(img);
+
+    var xPos = 0;
+    var yPos = 0;
+
+    var targetWidth = 300;
+    var targetHeight = targetWidth / imgWidth * imgHeight;
+
+    b.transformImage(img, xPos, yPos, targetWidth, targetHeight);
+    b.addPage();
+
+  }
+
+}
+
+b.go();
