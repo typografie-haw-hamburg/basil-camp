182b5d912d73f9c2460836b6cf42705f7572d01a
Doeroe <dorothee.schraudner@gmail.com>
2017-05-12 09:33:30 +0200

    Update 2.3_trianglePro.jsx

--- Scripts/Uebungen/2.3_trianglePro.jsx
+++ Scripts/Uebungen/2.3_trianglePro.jsx
@@ -6,26 +6,26 @@ function draw() {
 b.clear(b.doc());
 b.units(b.MM);
 
-var xOne = b.width/2;
-var yOne = b.height/2;
+  var xOne = b.width/2;
+  var yOne = b.height/2;
 
-var xTwo = 3 * b.width / 4;
-var yTwo = 3 * b.height / 4;
+  var xTwo = 3 * b.width / 4;
+  var yTwo = 3 * b.height / 4;
 
-var xThree = b.width / 4;
+  var xThree = b.width / 4;
 
-var myColor = b.color(200, 165, 110);
+  var myColor = b.color(200, 165, 110);
 
-b.fill(myColor);
-b.strokeWeight(4);
+  b.fill(myColor);
+  b.strokeWeight(4);
 
 
-b.beginShape(b.CLOSE)
-b.vertex(xOne, yOne);
-b.vertex(xTwo, yTwo);
-b.vertex(xThree, yTwo);
+  b.beginShape(b.CLOSE)
+  b.vertex(xOne, yOne);
+  b.vertex(xTwo, yTwo);
+  b.vertex(xThree, yTwo);
 
-b.endShape();
+  b.endShape();
 }
 
 b.go();
