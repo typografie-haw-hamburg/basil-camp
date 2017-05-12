6d0ac8421b96d93b11439926a9d430d03cadbf03
trych <timorychert@23x5.net>
2017-05-12 12:20:33 +0200

    sol

--- Scripts/Loesungen/2.3_trianglePro.jsx
+++ Scripts/Loesungen/2.3_trianglePro.jsx
@@ -6,8 +6,8 @@ function draw() {
   b.clear(b.doc());
   b.units(b.MM);
 
-  var xOne = b.width/2;
-  var yOne = b.height/2;
+  var xOne = b.width / 2;
+  var yOne = b.height / 2;
 
   var xTwo = 3 * b.width / 4;
   var yTwo = 3 * b.height / 4;
@@ -19,23 +19,13 @@ function draw() {
   b.fill(myColor);
   b.strokeWeight(4);
 
-  // triangle
-  b.beginShape(b.CLOSE)
+
+  b.beginShape(b.CLOSE);
   b.vertex(xOne, yOne);
   b.vertex(xTwo, yTwo);
   b.vertex(xThree, yTwo);
+
   b.endShape();
-
-
-  // typo teaser
-  var myText = "I LOVE MY TRIANGLE"; 
-  var xPos = 20; 
-  var yPos = 20; 
-  var widthTextframe = 150; 
-  var heightTextframe = 30; 
-
-  b.text(myText, xPos, yPos, widthTextframe, heightTextframe);
-
 }
 
 b.go();
--- Scripts/Loesungen/2.3_typoTeaser.jsx
+++ Scripts/Loesungen/2.3_typoTeaser.jsx
@@ -5,11 +5,11 @@ function draw() {
 
   b.clear(b.doc());
 
-  var myText = "I LOVE MY TRIANGLE"; 
-  var xPos = 20; 
-  var yPos = 20; 
-  var widthTextframe = 150; 
-  var heightTextframe = 30; 
+  var myText = "I LOVE MY TRIANGLE";
+  var xPos = 20;
+  var yPos = 20;
+  var widthTextframe = 150;
+  var heightTextframe = 30;
 
   b.text(myText, xPos, yPos, widthTextframe, heightTextframe);
 
